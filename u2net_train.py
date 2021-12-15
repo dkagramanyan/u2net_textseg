e22ec931fbf6a7e6f2e670c3447150216d59a935
David <avatar_4500@mail.ru>
2021-12-15 20:47:18 +0300

    added test dataloader

--- u2net_train.py
+++ u2net_train.py
@@ -27,7 +27,10 @@ from u2net import U2NET
 
 import time
 import datetime
-import gdown
+
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 # ------- 1. define loss function --------
 
@@ -52,10 +55,13 @@ def muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v):
 
 model_name = f'u2net_{datetime.datetime.now().date()}'
 
-images_path = 'textseg/image/'
-masks_path = 'textseg/semantic_label/'
+images_path = 'data/image/'
+masks_path = 'data/semantic_label/'
 
-model_dir = 'saved_models/' + model_name
+model_dir = 'saved_models/'
+
+if not os.path.exists(model_dir):
+    os.mkdir(model_dir)
 
 tra_img_name_list = glob.glob(images_path + '*')
 tra_lbl_name_list = glob.glob(masks_path + '*')
@@ -64,7 +70,7 @@ salobj_dataset = SalObjDataset(
     img_name_list=tra_img_name_list,
     lbl_name_list=tra_lbl_name_list,
     transform=transforms.Compose([
-        RescaleT(320),
+        RescaleT(500),
         #     RandomCrop(288),
         ToTensorLab(flag=0)])
 )
@@ -77,65 +83,88 @@ if torch.cuda.is_available():
 
 # ------- 4. define optimizer and train --------
 
-ite_num = 0
-running_loss = 0.0
-running_tar_loss = 0.0
-ite_num4val = 0
-save_frq = 2000  # save the model every 2000 iterations
+train_step = 0
+train_loss = 0.0
+test_loss = 0.0
+
+save_frq = 1  # save the model every 2000 iterations
 
 epoch_num = 1000
-batch_size_train = 24
+batch_size = 2
 train_num = len(tra_img_name_list)
-batch_size_val = 1
-val_num = 0
+validation_split = 0.2
 
-salobj_dataloader = DataLoader(salobj_dataset, batch_size=batch_size_train, shuffle=True, num_workers=1)
+train_dataset_size = int(train_num * (1 - validation_split))
+test_dataset_size = train_num - train_dataset_size
+
+train_dataset, test_dataset = torch.utils.data.random_split(salobj_dataset, (train_dataset_size, test_dataset_size))
+
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
 
 optimizer = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
 
 for epoch in range(0, epoch_num):
     net.train()
 
-    for i, data in enumerate(salobj_dataloader):
-        ite_num = ite_num + 1
-        ite_num4val = ite_num4val + 1
+    for i, train_data in enumerate(train_dataloader):
+        train_step += 1
+        train_step_save = + 1
 
-        inputs, labels = data['image'], data['label']
+        inputs, labels = train_data['image'], train_data['label']
 
         inputs = inputs.type(torch.FloatTensor)
         labels = labels.type(torch.FloatTensor)
 
-        # wrap them in Variable
-        if torch.cuda.is_available():
-            inputs_v, labels_v = Variable(inputs.cuda(), requires_grad=False), Variable(labels.cuda(),
-                                                                                        requires_grad=False)
-        else:
-            inputs_v, labels_v = Variable(inputs, requires_grad=False), Variable(labels, requires_grad=False)
+        inputs_v, labels_v = Variable(inputs.cuda(), requires_grad=False), Variable(labels.cuda(),
+                                                                                    requires_grad=False)
 
         # y zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
         d0, d1, d2, d3, d4, d5, d6 = net(inputs_v)
-        loss2, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
+        _, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
 
         loss.backward()
         optimizer.step()
 
         # # print statistics
-        running_loss += loss.data.item()
+        train_loss += loss.data.item()
 
         # del temporary outputs and loss
-        del d0, d1, d2, d3, d4, d5, d6, loss2, loss
+        del d0, d1, d2, d3, d4, d5, d6, loss
 
         print(
-            f"epoch: {epoch + 1}/{epoch_num}, batch: {(i + 1) * batch_size_train}/{train_num},"
-            f" iteration: {ite_num} train loss: {running_loss / ite_num4val} ")
+            f"epoch: {epoch + 1}/{epoch_num}, batch: {(i + 1) * batch_size}/{train_num},"
+            f" iteration: {train_step} train loss: {train_loss / train_step_save} ")
 
-        if ite_num % save_frq == 0:
-            torch.save(net.state_dict(), model_dir + model_name + "_bce_itr_%d_train_%3f_tar_%3f.pth" % (
-                ite_num, running_loss / ite_num4val, running_tar_loss / ite_num4val))
-            running_loss = 0.0
-            running_tar_loss = 0.0
+        if train_step % save_frq == 0:
+            print('testing')
+            with torch.no_grad():
+                for j, test_data in enumerate(test_dataloader):
+                    inputs, labels = test_data['image'], test_data['label']
+
+                    inputs = inputs.type(torch.FloatTensor)
+                    labels = labels.type(torch.FloatTensor)
+
+                    inputs_v, labels_v = Variable(inputs.cuda(), requires_grad=False), Variable(labels.cuda(),
+                                                                                                requires_grad=False)
+
+                    d0, d1, d2, d3, d4, d5, d6 = net(inputs_v)
+                    _, loss = muti_bce_loss_fusion(d0, d1, d2, d3, d4, d5, d6, labels_v)
+
+                    # # print statistics
+                    test_loss += loss.data.item()
+
+                    # del temporary outputs and loss
+                    del d0, d1, d2, d3, d4, d5, d6, loss
+
+            print(
+                f"epoch: {epoch + 1}/{epoch_num}, iteration: {train_step} test loss: {test_loss / test_dataset_size} ")
+
+            torch.save(net.state_dict(), model_dir + model_name + "_bce_itr_%d_train_%3f_test_%3f.pth" % (
+                train_step, train_loss / train_step_save, test_loss / int(train_num * validation_split)))
+            train_loss = 0.0
             net.train()  # resume train
-            ite_num4val = 0
+            train_step_save = 0
